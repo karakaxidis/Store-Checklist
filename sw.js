fbab3307631e9f1cf86da1d85f88485e5218d07b
karakaxidis <karakaxidis@gmail.com>
2026-03-24 13:36:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'store-checklist-cache-v4';
+const CACHE_NAME = 'store-checklist-cache-v5';
 const URLS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
